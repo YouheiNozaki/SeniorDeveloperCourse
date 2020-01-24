257a61b6fa5a99ccaff77f775fb7c824c96557c2
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-25 00:25:38 +0900

    Updates

---
+++ precache-manifest.3744723d30e69c13535da24f1b23ae69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2a2088b23c73181862a70b3f0a745f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8543d3312c2d3194b398",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d0dc041baa942a26b26c",
+    "url": "/static/css/main.8ef48d1c.chunk.css"
+  },
+  {
+    "revision": "8543d3312c2d3194b398",
+    "url": "/static/js/2.f8d5c122.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f8d5c122.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0dc041baa942a26b26c",
+    "url": "/static/js/main.b9d88433.chunk.js"
+  },
+  {
+    "revision": "fb20e1e569cb02e7a372",
+    "url": "/static/js/runtime-main.add1da58.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
