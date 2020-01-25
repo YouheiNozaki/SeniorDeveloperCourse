f55674e51996c9ff71451d2119dda9bd31bffcd3
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-25 23:54:22 +0900

    use hooks

--- robofriendsapp1/src/containers/App.js
+++ robofriendsapp1/src/containers/App.js
@@ -1,50 +1,48 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSearchField, requestRobots } from '../action';
 import './App.css';
 
-import { setSearchField, requestRobots } from '../action';
+const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
 
-const mapStateToProps = state => {
-  return {
-    searchField: state.searchRobots.searchField,
-    robots: state.requestRobots.robots,
-    isPending: state.requestRobots.isPending,
-    error: state.requestRobots.error,
+  const text = useSelector(state => state.searchRobots.setSearchField);
+  const robots = useSelector(state => state.requestRobots.robots);
+  const dispatch = useDispatch();
+
+  const onSearchChange = e => {
+    dispatch(setSearchField(e.target.value));
   };
-};
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots()),
-  };
-};
+  useEffect(() => {
+    dispatch(requestRobots());
+  }, [dispatch]);
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-
-  render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  useEffect(() => {
+    let filteredRobots = robots.filter(robots => {
+      return robots.name.toLowerCase().includes(text.toLowerCase());
     });
-    return isPending ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
-  }
-}
+    setSearchResults(filteredRobots);
+  }, [robots, text]);
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+  const newRobot = searchResults;
+
+  return (
+    <div className="tc">
+      <Scroll>
+        <h1 className="f2">RoboFriends</h1>
+        <SearchBox SearchChange={onSearchChange} />
+      </Scroll>
+      {text === '' ? (
+        <CardList robots={robots} />
+      ) : (
+        <CardList robots={newRobot} />
+      )}
+    </div>
+  );
+};
+
+export default App;
--- robofriendsapp1/src/index.js
+++ robofriendsapp1/src/index.js
@@ -10,7 +10,6 @@ import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
 const logger = createLogger();
-
 const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(
   rootReducer,
