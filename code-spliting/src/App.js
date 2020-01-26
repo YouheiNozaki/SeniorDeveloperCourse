6d28484e4c855fec0c677a1353b97ffc7f39fa77
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-26 15:56:11 +0900

    create asyncomponent

--- code-spliting/src/App.js
+++ code-spliting/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Page1 from './components/Page1';
+import AsyncComponent from './components/AsyncComponent';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       route: 'page1',
-      component: '',
+      component: null,
     };
   }
 
@@ -27,20 +28,22 @@ class App extends Component {
   };
 
   render() {
-    //   if (this.state.route === 'page1') {
-    //     return <Page1 onRouteChange={this.onRouteChange} />;
-    //   } else if (this.state.route === 'page2') {
-    //     return <Page2 onRouteChange={this.onRouteChange} />;
-    //   } else if (this.state.route === 'page3') {
-    //     return <Page3 onRouteChange={this.onRouteChange} />;
-    //   }
-    // }
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />;
-    } else {
-      return <this.state.component onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page2') {
+      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page3') {
+      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
     }
   }
+  //   if (this.state.route === 'page1') {
+  //     return <Page1 onRouteChange={this.onRouteChange} />;
+  //   } else {
+  //     return <this.state.component onRouteChange={this.onRouteChange} />;
+  //   }
+  // }
 }
 
 export default App;
