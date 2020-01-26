6e142847eeae72f28960f7cdf426305f8e7e1dcc
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-26 17:49:50 +0900

    fix bug

--- code-spliting/src/App.js
+++ code-spliting/src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import './App.css';
 
 import Page1 from './components/Page1';
-import AsyncComponent from './components/AsyncComponent';
+
+const Page2Lazy = React.lazy(() => import('./components/Page2'));
+const Page3Lazy = React.lazy(() => import('./components/Page3'));
 
 class App extends Component {
   constructor() {
@@ -31,19 +33,19 @@ class App extends Component {
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />;
     } else if (this.state.route === 'page2') {
-      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
-      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
-    } else if (this.state.route === 'page3') {
-      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
-      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page2Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
+      );
+    } else {
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page3Lazy onRouteChange={this.onRouteChange} />;
+        </Suspense>
+      );
     }
   }
-  //   if (this.state.route === 'page1') {
-  //     return <Page1 onRouteChange={this.onRouteChange} />;
-  //   } else {
-  //     return <this.state.component onRouteChange={this.onRouteChange} />;
-  //   }
-  // }
 }
 
 export default App;
