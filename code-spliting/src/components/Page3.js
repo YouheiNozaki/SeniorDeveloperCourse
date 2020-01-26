5cf26cde5fd31841f31d5f6ef188688835464dc2
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-26 15:32:37 +0900

    spliting code

--- code-spliting/src/components/Page3.js
+++ code-spliting/src/components/Page3.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../logo.svg';
 
-const Page3 = ({ onRouteChange }) => (
+export const Page3 = ({ onRouteChange }) => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
